19bb050f7654c8ded10a1409a17c9e43ccb2b420
filipg <gruczka.f@gmail.com>
2020-04-03 12:06:23 +0200

    added form form validation -> registration form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { ErrorStateMatcher } from '@angular/material';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    return control.parent.errors && control.parent.errors['notSame'];
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -12,6 +19,8 @@ export class RegisterComponent implements OnInit {
 
   form: FormGroup;
   loading: boolean;
+
+  matcher = new MyErrorStateMatcher();
 
   constructor(
     private fb: FormBuilder,
@@ -28,7 +37,14 @@ export class RegisterComponent implements OnInit {
       'email': ['', Validators.required],
       'password': ['', Validators.required],
       'confirm': ['', Validators.required]
-    });
+    }, {validators: this.checkPasswords});
+  }
+
+  private checkPasswords(group: FormGroup): {notSame: boolean} {
+    const password = group.get('password').value;
+    const confirmPassword = group.get('confirm').value;
+
+    return password === confirmPassword ? null : { notSame: true };
   }
 
   onSubmit() {
