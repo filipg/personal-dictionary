eee686b032253c447f50820b8df7c83aae9659c5
filipg <gruczka.f@gmail.com>
2020-04-07 18:34:05 +0200

    displaying overall quiz result

--- src/app/dictionary/test-yourself/quiz-result/quiz-result.component.ts
+++ src/app/dictionary/test-yourself/quiz-result/quiz-result.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { QuizResult } from 'src/app/interfaces/quiz.interface';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-quiz-result',
@@ -7,6 +9,9 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./quiz-result.component.css']
 })
 export class QuizResultComponent implements OnInit {
+
+  selectionQuizResult: QuizResult;
+  overalResultToDisplay = '';
 
   constructor(
     private dataService: DataService
@@ -17,7 +22,28 @@ export class QuizResultComponent implements OnInit {
   }
 
   private getQuizResult() {
-    this.dataService.quizResultSubject.subscribe(data => console.log(data));
+    this.dataService.quizResultSubject.pipe(
+      take(1)
+    ).subscribe(data => {
+      if (data !== null) {
+        if (data.abcdQuizMode) {
+          this.selectionQuizResult = data;
+          this.displaySelectionQuizResult();
+        } else {
+          this.displayTranslationQuizResult();
+        }
+      }
+    });
+  }
+
+  private displaySelectionQuizResult() {
+    const correct = this.selectionQuizResult.items.map(el => el.usersAnswer.correctAnswer);
+    this.overalResultToDisplay = `${correct.filter(el => el === true).length} / ${correct.length}`;
+    console.log(correct);
+  }
+
+  private displayTranslationQuizResult() {
+
   }
 
 }
